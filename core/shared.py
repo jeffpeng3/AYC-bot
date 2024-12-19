8d981bd6ed698ce9025529f5262ddcb98a9293c3
jeff <jeffpeng2012@gmail.com>
2024-12-20 00:22:38 +0800

    fix:  name client is not defined

--- core/shared.py
+++ core/shared.py
@@ -1,12 +1,14 @@
 from aiohttp import ClientSession
 
-client: ClientSession
+client: ClientSession | None = None
+
 
 async def get_client() -> ClientSession:
     global client
     if not client:
         client = ClientSession()
     return client
+
 
 async def close_client():
     global client
