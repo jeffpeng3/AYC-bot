8bdc4d9c05d004611d02f9cf6b04939f5f26543a
jeff <jeffpeng2012@gmail.com>
2024-12-19 23:03:27 +0800

    refactor: llm.py

--- cogs/llm.py
+++ cogs/llm.py
@@ -1,16 +1,30 @@
 from discord import Bot, Message, Thread, TextChannel
 from discord.ext.commands import Cog
-import google.generativeai as genai
+from google.generativeai import GenerativeModel, configure
+from google.generativeai import ChatSession
+from google.generativeai.types import ContentDict
 from google.ai.generativelanguage import HarmCategory, SafetySetting
+from copy import deepcopy
 
 llm_message = list[dict[str, str | list[str]]]
 
 
 class llm(Cog):
     def __init__(self, bot: Bot) -> None:
+        self.init_prompts: list[ContentDict] = [
+            ContentDict(
+                role="user",
+                parts=[
+                    "你是 HACHI，隸屬於 RK Music 旗下 LIVE UNION 的虛擬歌手，並與 King Record 簽約主流出道。在這之後的對話，請使用繁體中文回答"
+                ],
+            ),
+            ContentDict(
+                role="model", parts=["好的，我是HACHI。我會使用繁體中文跟你對話。"]
+            ),
+        ]
         self.bot: Bot = bot
-        self.chats: dict[int, llm_message] = {}
-        self.model = genai.GenerativeModel(
+        self.chats: dict[int, ChatSession] = {}
+        self.model = GenerativeModel(
             "gemini-1.5-flash-latest",
             safety_settings=(
                 {
@@ -30,22 +44,19 @@ class llm(Cog):
                     "threshold": SafetySetting.HarmBlockThreshold.BLOCK_NONE,
                 },
             ),
-            generation_config={"temperature": 0.5},
+            generation_config={"temperature": 0.5,'max_output_tokens': 900},
         )
-        genai.configure()
+        configure()
 
-    def append_message(self, thread_id: int, role: str, content: str) -> None:
-        if content.startswith("*"):
-            return
-        if thread_id not in self.chats:
-            self.chats[thread_id] = [
-                {"role": "user", "parts": ["請使用繁體中文回答"]},
-                {"role": "model", "parts": ["好的。我會使用繁體中文回答。"]},
-            ]
-        if self.chats[thread_id][-1]["role"] == role:
-            self.chats[thread_id][-1]["parts"].append(content)  # type: ignore
-        else:
-            self.chats[thread_id].append({"role": role, "parts": [content]})
+    # def restore_context(self, thread_id: int, role: str, content: str) -> None:
+    #     if content.startswith("*"):
+    #         return
+    #     if thread_id not in self.chats:
+    #         self.chats[thread_id] =
+    #     if self.chats[thread_id][-1]["role"] == role:
+    #         self.chats[thread_id][-1]["parts"].append(content)  # type: ignore
+    #     else:
+    #         self.chats[thread_id].append({"role": role, "parts": [content]})
 
     @Cog.listener("on_message")
     async def on_message(self, message: Message):
@@ -60,11 +71,10 @@ class llm(Cog):
             thread = await message.create_thread(name=msg, auto_archive_duration=60)
             with thread.typing():
                 try:
-                    self.append_message(thread.id, "user", msg)
-                    result = await self.model.generate_content_async(
-                        self.chats[thread.id]
+                    self.chats[thread.id] = self.model.start_chat(
+                        history=self.init_prompts
                     )
-                    self.append_message(thread.id, "model", result.text)
+                    result = await self.chats[thread.id].send_message_async(msg)
                     await thread.send(result.text)
                 except Exception as e:
                     await thread.send(str(e))
@@ -86,20 +96,17 @@ class llm(Cog):
                     return
                 if not msg.content.startswith("<@"):
                     return
+                history = deepcopy(self.init_prompts)
                 async for i in thread.history(limit=10):
                     if i.author.bot:
-                        self.append_message(thread.id, "model", i.content)
+                        history.append(ContentDict(role="model", parts=[i.content]))
                     else:
-                        self.append_message(thread.id, "user", i.content)
+                        history.append(ContentDict(role="user", parts=[i.content]))
+                self.chats[thread.id] = self.model.start_chat(history=history)
 
             with thread.typing():
                 try:
-                    msg = message.content.split(maxsplit=1)[-1]
-                    self.append_message(thread.id, "user", msg)
-                    result = await self.model.generate_content_async(
-                        self.chats[thread.id]
-                    )
-                    self.append_message(thread.id, "model", result.text)
+                    result = await self.chats[thread.id].send_message_async(msg)
                     await thread.send(result.text)
                 except Exception as e:
                     await thread.send(str(e))
