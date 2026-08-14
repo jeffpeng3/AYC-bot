2f3a914bc286a911978ec010319b20040c679f91
jeff <jeffpeng2012@gmail.com>
2026-08-14 22:08:01 +0800

    fix: 分流名稱改用 3 bits 均勻分布 8 字元

--- cogs/auto_mod.py
+++ cogs/auto_mod.py
@@ -5,7 +5,7 @@ from core.config import runtime
 
 ENTRY_CHANNEL_ID = 1153635272840458270
 VOICE_NAME_PREFIX = "分流"
-VOICE_CHARS = "↑↓←→"
+VOICE_CHARS = "↑↓←→↖↗↘↙"
 
 
 class auto_mod(Cog):
@@ -16,10 +16,10 @@ class auto_mod(Cog):
     async def on_voice(self, member: Member, before: VoiceState, after: VoiceState):
         if after.channel and after.channel.id == ENTRY_CHANNEL_ID:
             code = time_ns() + member.id
-            code = bin(code)[2:][-16:]
+            code = bin(code)[2:][-12:]
             name = ""
             for i in range(4):
-                name += VOICE_CHARS[int(code[i * 2 : 2 * (i + 1)], 2)]
+                name += VOICE_CHARS[int(code[i * 3 : 3 * (i + 1)], 2)]
             new_channel = await after.channel.clone(name=f"{VOICE_NAME_PREFIX}{name}", reason="分流")
             await new_channel.edit(rtc_region=VoiceRegion(runtime.last_region), nsfw=True)
             await member.move_to(new_channel)
